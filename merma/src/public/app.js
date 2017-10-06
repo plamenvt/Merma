2c3ef2de53b6cfd2a8180db3c9d811c486df8e40
plamenvt <plamenvto@gmail.com>
2017-10-06 12:24:32 +0300

    Sammy.js setup

--- merma/src/public/app.js
+++ merma/src/public/app.js
@@ -1,17 +1,44 @@
-/* globals $ Navigo controllers */
+/* globals $ */
 
 $(() => {
-    const loader = window.loader;
+    var sammyApp = Sammy('#content', function() {
+        
+        this.get('/', homeController.all);
+        
+        this.get('#/users', usersController.all);
+        this.get('#/users/register', usersController.register);
+    });
 
-    window.baseUrl = '/api/';
-    const root = null;
-    const useHash = false;
-
-    loader.init();
-
-    let router = new Navigo(root, useHash);
-
-    router
-        .on(controllers.home.initial)
-        .resolve();
+    $(function() {
+        sammyApp.run();
+    
+        // if (data.users.hasUser()) {
+        //   $('#container-sign-in').addClass('hidden');
+        //   $('#btn-sign-out').on('click', function(e) {
+        //     e.preventDefault();
+        //     data.users.signOut()
+        //       .then(function() {
+        //         document.location = '#/';
+        //         document.location.reload(true);
+        //       });
+        //   });
+        // } else {
+        //   $('#container-sign-out').addClass('hidden');
+        //   $('#btn-sign-in').on('click', function(e) {
+        //       e.preventDefault();
+        //     var user = {
+        //       username: $('#tb-username').val(),
+        //       password: $('#tb-password').val()
+        //     };
+        //     data.users.signIn(user)
+        //       .then(function(user) {
+        //         document.location = '#/';
+        //         document.location.reload(true);
+        //       }, function(err) {
+        //         $('#container-sign-in').trigger("reset");
+        //         toastr.error(err.responseText);
+        //       });
+        //   });
+        // }
+    });
 });
