84d6dd21b4bd47889fed44971c949003166fe3ed
plamenvt <plamenvto@gmail.com>
2017-10-23 22:55:51 +0300

    profile header remove

--- merma/src/public/controllers/profileController.js
+++ merma/src/public/controllers/profileController.js
@@ -3,6 +3,12 @@ const profileController = function() {
     function all(context) {
     templates.get('profile')
         .then(function(template) {
+            const body = $('body').removeClass('background-home');
+            body.addClass('background-standard');
+            const header = $('header').removeClass('header-home');
+            header.addClass('header-standard');
+            $('#carousel-container').addClass('hidden');
+            $('#category-name').addClass('hidden');
             context.$element().html(template());
         });
     }
